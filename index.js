31e21094ebb67f6183a7f3ec314360bc6ea1308a
moses <moses.kingi@moringaschool.com>
2023-11-01 22:05:12 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+
 // Your code here
+
+class Polygon {
+    constructor(sides) {
+      this.sides = sides;
+    }
+  
+    get countSides() {
+      return this.sides.length;
+    }
+  
+    get perimeter() {
+      return this.sides.reduce((sum, side) => sum + side, 0);
+    }
+  }
+  //borrows the properties of its parent class polygon and he sum of the lengths of any two sides of a triangle is greater than the length of the third side //
+  
+  class Triangle extends Polygon {
+    get isValid() {
+      if (this.countSides !== 3) return false;
+      const [a, b, c] = this.sides;
+      return a + b > c && a + c > b && b + c > a;
+    }
+  }
+  
+  class Square extends Polygon {
+    get isValid() {
+      if (this.countSides !== 4) return false;
+      const [a, b, c, d] = this.sides;
+      return a === b && b === c && c === d;
+    }
+  
+    get area() {
+      const [a, b] = this.sides;
+      return a * b;
+    }
+  }
+  
